05b910e1b13a9da45703a8618cbb6ab4fbba275d
deco-L <scotchwitchbroom@gmail.com>
2024-01-17 09:56:10 +0900

    print data

--- srcs/main/main.c
+++ srcs/main/main.c
@@ -6,31 +6,36 @@
 /*   By: csakamot <csakamot@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/11 13:32:08 by csakamot          #+#    #+#             */
-/*   Updated: 2024/01/17 06:15:32 by csakamot         ###   ########.fr       */
+/*   Updated: 2024/01/17 09:55:34 by csakamot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 // #include <libc.h>
 #include "../../inc/cub3d.h"
 
+static void	print_map_variable(t_data *data)
+{
+	printf("\n---texture path---\n");
+	printf("%s: %s\n", NO, data->map->no_texture);
+	printf("%s: %s\n", SO, data->map->so_texture);
+	printf("%s: %s\n", WE, data->map->we_texture);
+	printf("%s: %s\n", EA, data->map->ea_texture);
+	printf("\n---rgb_color---\n");
+	for (int index = 0; data->map->fl_color[index] != -1; index++)
+		printf(" %s[%d]: %d", FL, index, data->map->fl_color[index]);
+	printf("\n");
+	for (int index = 0; data->map->ce_color[index] != -1; index++)
+		printf(" %s[%d]: %d", CE, index, data->map->ce_color[index]);
+	printf("\n\n---map---\n");
+	for (int index = 0; data->map->map[index] != NULL; index++)
+		printf("%s\n", data->map->map[index]);
+}
+
 int	main(int argc, char **argv)
 {
 	t_data	data;
 
 	init_map(&data, argc, argv);
+	print_map_variable(&data);
 	return (EXIT_SUCCESS);
 }
-
-// void	print_map_variable(t_data *data)
-// {
-// 	printf("%s: %s\n", NO, data->map->no_texture);
-// 	printf("%s: %s\n", SO, data->map->so_texture);
-// 	printf("%s: %s\n", WE, data->map->we_texture);
-// 	printf("%s: %s\n", EA, data->map->ea_texture);
-// 	for (int index = 0; data->map->fl_color[index] != -1; index++)
-// 		printf("%s[%d]: %d\n", FL, index, data->map->fl_color[index]);
-// 	for (int index = 0; data->map->ce_color[index] != -1; index++)
-// 		printf("%s[%d]: %d\n", CE, index, data->map->ce_color[index]);
-// 	for (int index = 0; data->map->map[index] != NULL; index++)
-// 		printf("%s\n", data->map->map[index]);
-// }
