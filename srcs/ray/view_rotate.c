e63491f0ca2a1886f4eb71f18e302a147a4aab5b
deco-L <scotchwitchbroom@gmail.com>
2024-02-05 19:32:21 +0900

    Viewpoint change Viewpoint shift

--- srcs/ray/view_rotate.c
+++ srcs/ray/view_rotate.c
@@ -6,7 +6,7 @@
 /*   By: csakamot <csakamot@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/11 13:32:08 by csakamot          #+#    #+#             */
-/*   Updated: 2024/02/05 16:10:41 by csakamot         ###   ########.fr       */
+/*   Updated: 2024/02/05 19:30:30 by csakamot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,7 @@ void	view_point_rotate(t_data *data, bool left, bool right)
 	data->game->dirx = VECTOR * cos(get_radian(*angle));
 	data->game->diry = -1 * (VECTOR * sin(get_radian(*angle)));
 	data->game->camx = CAMERA / 2 * sin(get_radian(*angle));
-	data->game->camy = CAMERA / 2 * -1 * cos(get_radian(*angle));
+	data->game->camy = CAMERA / 2 * cos(get_radian(*angle));
 	ray_cast(data);
 	return ;
 }
