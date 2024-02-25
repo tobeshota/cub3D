c2efbb3276bc273c323ef60efaeab6a08660f0fc
deco-L <scotchwitchbroom@gmail.com>
2024-02-25 16:09:44 +0900

    V V

--- srcs/check/check_map.c
+++ srcs/check/check_map.c
@@ -6,7 +6,7 @@
 /*   By: csakamot <csakamot@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/11 13:32:08 by csakamot          #+#    #+#             */
-/*   Updated: 2024/01/27 16:30:11 by csakamot         ###   ########.fr       */
+/*   Updated: 2024/02/25 16:01:39 by csakamot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,30 @@ static bool	empty_map_check(t_map *map)
 	return (false);
 }
 
+bool	player_check(char **map)
+{
+	int	x;
+	int	y;
+	int	count;
+
+	y = 0;
+	count = 0;
+	while (map[y] != NULL)
+	{
+		x = 0;
+		while (map[y][x] != '\0')
+		{
+			if (judge_directon_chara(map[y][x]))
+				count++;
+			x++;
+		}
+		y++;
+	}
+	if (count != 1)
+		return (print_error_msg("Invalid map."), true);
+	return (false);
+}
+
 bool	check_map(t_map *map)
 {
 	if (empty_map_check(map))
@@ -26,5 +50,7 @@ bool	check_map(t_map *map)
 	if (bfs_map_check(map))
 		return (false);
 	retrun_bfs_map(map);
+	if (player_check(map->map))
+		return (false);
 	return (true);
 }
